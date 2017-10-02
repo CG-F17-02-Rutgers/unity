a4d23692ec2c4abface0c4da1ccf8479b9e7a597
ericyu049 <ericyu049@gmail.com>
2017-10-01 23:01:25 -0400

    Signed-off-by: ericyu049 <ericyu049@gmail.com>

---
+++ Assets/Scripts/Controllers/AgentController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour{
+    NavMeshAgent agent;
+
+    void Start(){
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        /*
+        if (Input.GetMouseButtonDown(0)){
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)){
+                agent.destination = hit.point;
+            }
+        }
+        */
+    }
+    void Move(Vector3 position){
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)){
+            agent.destination = hit.point;
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Director.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)){
+            Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rhInfo;
+            bool didHit = Physics.Raycast(toMouse, out rhInfo);
+            if (didHit)
+            {
+                Debug.Log(rhInfo.collider.name + "---" + rhInfo.point);
+                AgentController destScript = rhInfo.collider.GetComponent<AgentController>();
+                if (destScript)
+                {
+                }
+            }
+            else
+            {
+                Debug.Log("clicked on EmptySpace");
+            }
+
+        }
+	}
+}
