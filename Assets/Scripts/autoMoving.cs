a6b05839c02596d194a3190a82a0da7841287321
CG-F17-02-Rutgers <sitian1995@gmail.com>
2017-10-02 14:09:55 -0400

    auto moving done i think

---
+++ Assets/Scripts/autoMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoMoving : MonoBehaviour {
+	public float speed = 2.0f;
+	private bool dirRight;
+
+	void Update () {
+		if(transform.position.x >= -78.0f) {
+			dirRight = false;
+		}
+
+		if(transform.position.x <= -120.0f){
+			dirRight = true;
+		}
+		if (dirRight) {
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+		} 
+		else {
+			transform.Translate (-Vector2.right * speed * Time.deltaTime);
+		}
+	}
+}
