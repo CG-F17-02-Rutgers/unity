8257c04d14ac2de67edb98693636a0ed73836489
DESKTOP-5E26GU4\Michael <michael.sulit@gmail.com>
2017-10-13 03:05:33 -0400

    basic third person functionality

---
+++ Assets/B2/CameraFollower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 targetOffset;
+
+    public float cameraDistance = 3.0f;
+    public float maxY =  60.0f;
+    public float minY = -60.0f;
+
+    private float camX = 0;
+    private float camY = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        camX += mouseX;
+        camY -= mouseY;
+
+        camY = Mathf.Clamp(camY, minY, maxY);
+
+        Quaternion rot = Quaternion.Euler(camY, camX, 0);
+        transform.position = target.position + targetOffset;
+        transform.rotation = rot;
+        transform.Translate(Vector3.back * cameraDistance, Space.Self);
+
+    }
+}
---
+++ Assets/B2/ThirdPersonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour {
+
+    public Transform forwardReference;
+    
+    private Animator animator;
+
+    private bool running = false;
+
+    private int speedID;
+    private int turningID;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        speedID = Animator.StringToHash("Speed");
+        turningID = Animator.StringToHash("Turning");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        //float angle = Vector3.Angle(Vector3.ProjectOnPlane(transform.forward, Vector3.up), Vector3.ProjectOnPlane(forwardReference.forward, Vector3.up));
+        //float angle = Vector3.SignedAngle(transform.forward, forwardReference.forward, Vector3.up);
+        //angle = Mathf.Clamp(angle, -1, 1);
+        //Debug.Log(angle);
+
+        //angle = Mathf.Clamp(angle / 10.0f, -1, 1);
+        //animator.SetFloat(turningID, angle);
+
+        //transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(forwardReference.forward,Vector3.up));
+
+        if (x > 0.1f) {
+            animator.SetFloat(turningID, -1.0f, 0.1f, Time.deltaTime);
+        } else if (x < -0.1f) {
+            animator.SetFloat(turningID, 1.0f, 0.1f, Time.deltaTime);
+        } else {
+            animator.SetFloat(turningID, 0.0f, 0.1f, Time.deltaTime);
+        }
+
+        float moveSpeed;
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            moveSpeed = 1.0f;
+        } else {
+            moveSpeed = 0.5f;
+        }
+
+        if(y > 0.1f) {
+            animator.SetFloat(speedID, moveSpeed, 0.1f, Time.deltaTime);
+        } else {
+            animator.SetFloat(speedID, 0.0f, 0.1f, Time.deltaTime);
+        }
+    }
+}
