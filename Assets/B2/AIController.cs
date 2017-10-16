a8f3c843bbc0fd19bb53346e85d83655b66610c5
DESKTOP-5E26GU4\Michael <michael.sulit@gmail.com>
2017-10-16 12:52:11 -0400

    added AI control functionality

---
+++ Assets/B2/AIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+
+public class AIController : MonoBehaviour {
+
+    private Animator animator;
+    private NavMeshAgent navAgent;
+
+    private int speedID;
+    private int turningID;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+
+        speedID = Animator.StringToHash("Speed");
+        turningID = Animator.StringToHash("Turning");
+
+        navAgent.updatePosition = false;
+        navAgent.updateRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 deltaPosition = navAgent.nextPosition - transform.position;
+        float forward = Vector3.Dot(deltaPosition, transform.forward);
+        forward = forward / Time.deltaTime;
+        float side = Vector3.Dot(deltaPosition, transform.right);
+        side = side / Time.deltaTime;
+        animator.SetFloat(turningID, side, 0.001f, Time.deltaTime);
+        animator.SetFloat(speedID, forward, 0.001f, Time.deltaTime);
+    }
+
+    private void OnAnimatorMove() {
+
+        //navAgent.steeringTarget
+        transform.position = navAgent.nextPosition;
+    }
+}
---
+++ Assets/B2/RTSStyleCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSStyleCamera : MonoBehaviour {
+
+    public float cameraSpeed = 0.5f;
+
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * cameraSpeed;
+        float y = Input.GetAxis("Vertical") * cameraSpeed;
+
+        transform.Translate(x, 0, y, Space.World);
+	}
+}
